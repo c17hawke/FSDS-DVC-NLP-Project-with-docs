e851a547f440d47e6854ded7ba8affd0e48698d5
c17hawke <sunny.c17hawke@gmail.com>
2022-06-18 18:35:29 +0530

    stage 02 mid

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -1,11 +1,11 @@
 import argparse
 import os
-import shutil
+import numpy as np
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, get_df
 import random
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 STAGE = "Featurization" ## <<< change stage name 
 
@@ -21,8 +21,46 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
 
+    artifacts = config["artifacts"]
+    prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
+    train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
+
+    featurized_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    create_directories([featurized_data_dir_path])
+
+    featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_DATA_TRAIN"])
+    featurized_test_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_DATA_TEST"])
+
+    max_features = params["featurize"]["max_features"]
+    n_grams = params["featurize"]["n_grams"]
+
+    # train
+    df_train = get_df(train_data_path)
+    train_words = np.array(df_train.text.str.lower().values.astype("U"))
+
+    bag_of_words = CountVectorizer(
+        stop_words="english",
+        max_features=max_features,
+        ngram_range=(1, n_grams)
+    )
+
+    bag_of_words.fit(train_words)
+    train_words_binary_matrix = bag_of_words.transform(train_words)
+
+    tfidf = TfidfTransformer(smooth_idf=False)
+    tfidf.fit(train_words_binary_matrix)
+    train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
+    # call a function to save this matrix
+
+    # for test data
+    df_test = get_df(test_data_path)
+    test_words = np.array(df_test.text.str.lower().values.astype("U"))
+    test_words_binary_matrix = bag_of_words.transform(test_words)
+    test_words_tfidf_matrix = tfidf.transform(test_words_binary_matrix)
+    # call a function to save this matrix
+    
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/common.py
+++ src/utils/common.py
@@ -22,3 +22,19 @@ def save_json(path: str, data: dict) -> None:
         json.dump(data, f, indent=4)
 
     logging.info(f"json file saved at: {path}")
+
+
+def get_df(
+    path_to_data: str, 
+    sep: str="\t", 
+    column_names: list=["id", "label", "text"],
+    encoding='utf-8') -> pd.DataFrame:
+    df = pd.read_csv(
+        path_to_data, 
+        delimiter=sep, 
+        encoding=encoding, 
+        header=None, 
+        names=column_names,
+    )
+    logging.info(f"The input data frame {path_to_data} of size {df.shape} is read.")
+    return df
